2b2d788a1c5ab5f8b8aa7b4f4ad90e82df30fc5a
Udit Arora <udit@Udits-MacBook-Pro.local>
2015-06-03 23:04:09 +0530

    Fixed grid layout.

--- osask.py
+++ osask.py
@@ -6,13 +6,7 @@ from kivy.uix.textinput import TextInput
 class MainMenuScreen(GridLayout):
     def __init__(self, **kwargs):
         super(MainMenuScreen, self).__init__(**kwargs)
-        self.cols = 2
-        # self.add_widget(Label(text='User Name'))
-        # self.username = TextInput(multiline=False)
-        # self.add_widget(self.username)
-        # self.add_widget(Label(text='password'))
-        # self.password = TextInput(password=True, multiline=False)
-        # self.add_widget(self.password)
+        self.rows = 5
 
 class OSASK(App):
     def build(self):
