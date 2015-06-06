55ee5815a5442d3d379e1d8bd2ca4b1b33e15e0f
Udit Arora <udit@Udits-MacBook-Pro.local>
2015-06-06 20:43:30 +0530

    Implemented multiple screens

--- osask.py
+++ osask.py
@@ -1,16 +1,23 @@
 from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
 
-class MainMenuScreen(GridLayout):
-    def __init__(self, **kwargs):
-        super(MainMenuScreen, self).__init__(**kwargs)
-        self.rows = 5
+Builder.load_file('osask.kv')
+
+class MainMenuScreen(Screen):
+    pass
+
+class CPUSchedulingInputScreen(Screen):
+	pass
+
+# Create the screen manager
+sm = ScreenManager()
+sm.add_widget(MainMenuScreen(name='menu'))
+sm.add_widget(CPUSchedulingInputScreen(name='cpu1'))
 
 class OSASK(App):
     def build(self):
-        return MainMenuScreen()
+        return sm
 
 if __name__ == '__main__':
     OSASK().run()
