3f4c379c5ab2ad0d5002c57626a112c7d49cab88
Udit Arora <udit@Udits-MacBook-Pro.local>
2015-06-07 01:44:16 +0530

    Added data bindings for fcfs scheduling form.

--- osask.py
+++ osask.py
@@ -6,14 +6,27 @@ from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import ObjectProperty, NumericProperty, StringProperty
+from functools import partial
 
 Builder.load_file('osask.kv')
+data_cpu = {}
+
+def on_name(instace, value, i):
+    data_cpu['name'+str(i)] = value
+
+def on_arrival(instace, value, i):
+    data_cpu['arrival'+str(i)] = value
+
+def on_burst(instace, value, i):
+    data_cpu['burst'+str(i)] = value
 
 class MainMenuScreen(Screen):
     pass
 
 class CPUSchedulingScreen(Screen):
     num_processes = ObjectProperty(None)
+
+    # Load the form for input
     def load_form(self, *args):
         layout = self.manager.get_screen('fcfs1').layout
         layout.clear_widgets()
@@ -23,17 +36,25 @@ class CPUSchedulingScreen(Screen):
             box = BoxLayout(orientation='horizontal')
             sno_label = Label(text=str(i+1))
             box.add_widget(sno_label)
+
             inp = TextInput(id='name'+str(i))
+            inp.bind(text=partial(on_name, i=i))
             box.add_widget(inp)
+
             inp = TextInput(id='arrival'+str(i))
+            inp.bind(text=partial(on_arrival, i=i))
             box.add_widget(inp)
+
             inp = TextInput(id='burst'+str(i))
+            inp.bind(text=partial(on_burst, i=i))
             box.add_widget(inp)
+
             layout.add_widget(box)
-            # layout.add_widget(Button(text=str(i+1))) 
 
 class FCFSInputScreen(Screen):
-	layout = ObjectProperty(None)
+    layout = ObjectProperty(None)
+    def show_data(self, *args):
+        print str(data_cpu)
 
 # Create the screen manager
 sm = ScreenManager()
