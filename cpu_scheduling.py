40b4307c09ee6f38e3e8c735340ea8038b2b8b16
Rohit <rohit.takhar1994@gmail.com>
2015-06-14 10:26:57 -0700

    Latest

--- cpu_scheduling.py
+++ cpu_scheduling.py
@@ -33,7 +33,7 @@ def fcfs(data):
 
     return process_chart, stats 
 
-def round_robin(data,max_quanta=20):
+def round_robin(data,max_quanta=4):
 	time_present = 0
     	wait_time = 0
     	turn_time = 0
@@ -117,16 +117,11 @@ def round_robin(data,max_quanta=20):
 			
 		if q.empty():
 			break
-	for data in details_process:
-		print str(data['end']) +" "+str(data['start'])+" "+str(data['arrival'])
-		
-
 
 	for data in details_process:
     		wait_time += ((data['start'] - data['arrival']) + (data['end'] - (data['start'] + data['burst'])))
     		turn_time += (data['end'] - data['arrival'])
     		sum_time += data['burst']
-		print str(wait_time) +" "+str(turn_time)+" "+str(sum_time)
 
 	curr_time = time_present
 	stats = {}
@@ -141,7 +136,11 @@ def round_robin(data,max_quanta=20):
 def shortest_job_non_prempted(data):
 	all_processes = sorted(data, key=itemgetter('burst'))
 	time_present = 0
+    	wait_time = 0
+    	turn_time = 0
+    	sum_time = 0
 	var = 0
+	completed_processes = list()
 	process_chart = []
 	while True:
 		for process in all_processes:
@@ -152,7 +151,7 @@ def shortest_job_non_prempted(data):
 				time_present += process['burst']
 				chart_details['end'] = time_present
 				process['burst'] = 0
-				all_processes.remove(process)
+				completed_processes.append(process)
 				if len(process_chart) > 0:
 					temp_dict = process_chart[-1]
 					if temp_dict['name'] == chart_details['name']:
@@ -166,7 +165,7 @@ def shortest_job_non_prempted(data):
 					time_present += 1
 					var = 0
 
-		if len(all_processes) == 0:	
+		if len(all_processes) == len(completed_processes):	
 			break
 	return process_chart
 	
@@ -206,7 +205,41 @@ def shortest_job_prempted(data):
 
 	return process_chart
 
+def priority_non_preemptive(data):
+	all_processes = list()
+	all_processes = sorted(data, key=itemgetter('priority'))
+	time_present = 0
+	var = 0
+	process_chart = list()
+	completed_processes = list()
+	while True:
+		for process in all_processes:
+			chart_details = {}
+			if process['burst'] > 0 and process['arrival'] <= time_present:
+				chart_details['name'] = process['name']
+				chart_details['start'] = time_present
+				time_present += process['burst']
+				chart_details['end'] = time_present
+				process['burst'] = 0
+				completed_processes.append(process)
+				if len(process_chart) > 0:
+					temp_dict = process_chart[-1]
+					if temp_dict['name'] == chart_details['name']:
+						chart_details['start'] = temp_dict['start']
+						del process_chart[-1]
+				process_chart += [chart_details]
+				var = 0
+			else:
+				var += 1
+				if var == len(all_processes):
+					time_present += 1
+					var = 0
 
+		if len(completed_processes) == len(all_processes):	
+			break
+	return process_chart
+	
+	
 ###### Test code ######
 
 list_process_round_robin = list()
@@ -217,28 +250,33 @@ list_process_round_robin = list()
 
 process = {}
 process['name'] = 1
-process['burst'] = 10
+process['burst'] = 5
 process['arrival'] = 2
+process['priority'] = 1
 list_process_round_robin += [process]
 process = {}
 process['name'] = 2
-process['burst'] = 5
+process['burst'] = 9
 process['arrival'] = 1
+process['priority'] = 2
 list_process_round_robin += [process]
 process = {}
 process['name'] = 3
-process['burst'] = 3
-process['arrival'] = 0
+process['burst'] = 4
+process['arrival'] = 3
+process['priority'] = 3 
 list_process_round_robin += [process]
 process = {}
 process['name'] = 4
-process['burst'] = 5
-process['arrival'] = 2
+process['burst'] = 7
+process['arrival'] = 5
+process['priority'] = 4
 list_process_round_robin += [process]
 process = {}
 process['name'] = 5
-process['burst'] = 10
-process['arrival'] = 3
+process['burst'] = 3
+process['arrival'] = 2
+process['priority'] = 5
 list_process_round_robin += [process]
 
 # process = {}
@@ -285,7 +323,8 @@ list_process_round_robin += [process]
 # list_process_shortest_job_prempted += [process]
 # #val = shortest_job_non_prempted(list_process_shortest_job_non_prempted)
 # val = shortest_job_prempted(list_process_shortest_job_prempted)
-xyz,x = round_robin(list_process_round_robin)
-print str(x['cpu_utilization'])+ " "+str(x['wait_time'])+ " "+str(x['sum_time']) + " " + str(x['throughput']) + " "+str(x['turn_time'])
+xyz,p= round_robin(list_process_round_robin)
+for x in xyz:
+	print str(x['name'])
 
 
