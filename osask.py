b963436852d00224f3288a9a827b5a4adc973d65
Udit Arora <uditarora09@gmail.com>
2015-07-02 00:05:06 +0530

    Added Deadlock Avoidance input form screen. Addeed docs built using pycco.

--- osask.py
+++ osask.py
@@ -1,4 +1,4 @@
-# kivy
+# Kivy libraries
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
@@ -10,101 +10,79 @@ from kivy.uix.popup import Popup
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import ObjectProperty, NumericProperty, StringProperty
 from kivy.graphics import Color, Rectangle, Line
-# python libraries
+# Python libraries
 from functools import partial
 from random import random
-# algorithms
+# OS Algorithms
 import cpu_scheduling
 
 Builder.load_file('layout.kv')
+
+# Global data for CPU Scheduling agorithms
 cpu_scheduling_types = ['FCFS', 'Round Robin', 'SJF Non-Preemptive', 'SJF Preemptive', 'Priority Non-Preemptive', 'Priority Preemptive']
 cpu_scheduling_type = 0
-data_cpu = {}
+data_cpu = dict()
 
-def on_name(instace, value, i):
+# Global data for Deadlock Avoidance algorithms
+data_da = dict()
+
+# Binder functions for CPU Scheduling algorithms form, to store data in the global 'data_cpu' dictionary
+def cpu_on_name(instace, value, i):
     if value == '':
         value = 'P'+str(i+1)
     data_cpu['name'+str(i)] = value
-
-def on_arrival(instace, value, i):
+def cpu_on_arrival(instace, value, i):
     if value == '':
         value = 1
     data_cpu['arrival'+str(i)] = value
-
-def on_burst(instace, value, i):
+def cpu_on_burst(instace, value, i):
     if value == '':
         value = 4
     data_cpu['burst'+str(i)] = value
-
-def on_priority(instace, value, i):
+def cpu_on_priority(instace, value, i):
     if value == '':
         value = 0
     data_cpu['priority'+str(i)] = value
-
-def on_quantum(instace, value):
+def cpu_on_quantum(instace, value):
     if value == '':
         value = 2
     data_cpu['quantum'] = int(value)
-
-def on_aging(instace, value):
+def cpu_on_aging(instace, value):
     if value == '':
         value = 4
     data_cpu['aging'] = int(value)
 
+# Binder functions for Dead Avoidance algorithm form
+def da_on_available(instace, value, i):
+    if (value == ''):
+        value = 5
+    data_da['available'+str(i)] = value
+def da_on_request(instance, value, i):
+    if (value == ''):
+        value = 0
+    data_da['request'+str(i)] = value
+def da_on_max(instance, value, i, j):
+    if (value == ''):
+        value = 10
+    if 'max' not in data_da:
+        data_da['max'] = dict()
+    if i not in data_da['max']:
+        data_da['max'][i] = dict()
+    data_da['max'][i][j] = value
+def da_on_allocation(instance, value, i, j):
+    if (value == ''):
+        value = 10
+    if 'allocation' not in data_da:
+        data_da['allocation'] = dict()
+    if i not in data_da['allocation']:
+        data_da['allocation'][i] = dict()
+    data_da['allocation'][i][j] = value
+
+# Main Menu Screen with options to choose an OS Algorithm
 class MainMenuScreen(Screen):
     pass
 
-class CPUSchedulingScreen(Screen):
-    num_processes = ObjectProperty(None)
-    cpu_type = 0
-    # Load the form for input
-    # def load_form(self, *args):
-    #     global cpu_scheduling_type;
-    #     cpu_scheduling_type = self.cpu_type
-    #     cpu_scheduling_type_text = cpu_scheduling_types[self.cpu_type]
-    #     type_text = self.manager.get_screen('cpu2').type_text
-    #     type_text.clear_widgets()
-    #     label = Label(text=str(cpu_scheduling_type_text)+' CPU Scheduling')
-    #     type_text.add_widget(label)
-
-    #     layout = self.manager.get_screen('cpu2').layout
-    #     layout.clear_widgets()
-    #     if (self.num_processes.text == "" or int(self.num_processes.text) == 0):
-    #         self.num_processes.text = "5"
-    #     data_cpu['num_processes'] = int(self.num_processes.text)
-
-    #     for i in range(int(self.num_processes.text)):
-    #         box = BoxLayout(orientation='horizontal')
-    #         sno_label = Label(text=str(i+1))
-    #         box.add_widget(sno_label)
-
-    #         # process names
-    #         inp = TextInput(id='name'+str(i))
-    #         inp.bind(text=partial(on_name, i=i))
-    #         box.add_widget(inp)
-    #         # arrival times
-    #         inp = TextInput(id='arrival'+str(i))
-    #         inp.bind(text=partial(on_arrival, i=i))
-    #         box.add_widget(inp)
-    #         # burst times
-    #         inp = TextInput(id='burst'+str(i))
-    #         inp.bind(text=partial(on_burst, i=i))
-    #         box.add_widget(inp)
-
-    #         layout.add_widget(box)
-    #     if self.cpu_type == 1:
-    #         box = BoxLayout(orientation='horizontal')
-    #         inp = TextInput(id='quantum')
-    #         inp.bind(text=on_quantum)
-    #         label = Label(text='Time quantum')
-    #         box.add_widget(label)
-    #         box.add_widget(inp)
-    #         layout.add_widget(box)
-
-class CPUInputScreen_old(Screen):
-    def show_data(self, *args):
-        print str(data_cpu)
-
+# Input Screen for CPU Scheduling Algorithms
 class CPUInputScreen(Screen):
     layout = ObjectProperty(None)
     cpu_type = 0
@@ -113,10 +91,12 @@ class CPUInputScreen(Screen):
         layout = self.manager.get_screen('cpu_form').layout
         self.layout.bind(minimum_height=self.layout.setter('height'))
 
+    # Binder function for algorithm type selection from Spinner (Dropdown)
     def bind_spinner(self, *args):
         spinner = self.manager.get_screen('cpu_form').algo_spinner
         spinner.bind(text=self.show_selected_value)
 
+    # Call set_cpu_type method with appropriate index of scheduling algorithm
     def show_selected_value(self, spinner, text, *args):
         if text == 'First Come First Serve':
             self.set_cpu_type(0)
@@ -127,7 +107,7 @@ class CPUInputScreen(Screen):
         elif text == 'Round Robin':
             self.set_cpu_type(1)
 
-    # Called when a new value is chosen from spinner
+    # Called when a new value is chosen from spinner. Sets cpu_type to the appropriate index in the cpu_scheduling_types list
     def set_cpu_type(self, new_cpu_type, *args):
         global cpu_scheduling_type
         cpu_scheduling_type = new_cpu_type
@@ -146,10 +126,10 @@ class CPUInputScreen(Screen):
             self.cpu_type = new_cpu_type
         self.load_form()
 
-    # Called when preemptive or non-preemtive option is clicked
+    # Called when preemptive or non-preemtive option is clicked. Sets cpu_type to the appropriate index in the cpu_scheduling_types list
     def update_cpu_type(self, *args):
         global cpu_scheduling_type
-        # If FCFS or RR
+        # If FCFS or RR Scheduling
         if self.cpu_type == 0 or self.cpu_type == 1:
             pass
         elif self.preemptive_flag == True and self.cpu_type%2 == 0:
@@ -158,15 +138,10 @@ class CPUInputScreen(Screen):
         elif self.preemptive_flag == False and self.cpu_type%2 != 0:
             self.cpu_type -= 1
             cpu_scheduling_type = self.cpu_type
-        # print 'updated cpu_type:', self.cpu_type
 
+    # Load the appropriate form inputs according to the CPU Scheduling algorithm selected
     def load_form(self, *args):
-        # cpu_scheduling_type_text = cpu_scheduling_types[self.cpu_type]
-        # type_text = self.manager.get_screen('cpu2').type_text
-        # type_text.clear_widgets()
-        #label = Label(text=str(cpu_scheduling_type_text)+' CPU Scheduling')
-        #type_text.add_widget(label)
-
+        # Layout is the area where the form is placed on the screen
         layout = self.manager.get_screen('cpu_form').layout
         layout.clear_widgets()
         if (self.num_processes.text == "" or int(self.num_processes.text) == 0):
@@ -198,24 +173,21 @@ class CPUInputScreen(Screen):
 
             # process names
             inp = TextInput(id='name'+str(i))
-            inp.bind(text=partial(on_name, i=i))
-            # inp.font_size = inp.size[1]
+            inp.bind(text=partial(cpu_on_name, i=i))
             box.add_widget(inp)
             # arrival times
             inp = TextInput(id='arrival'+str(i))
-            inp.bind(text=partial(on_arrival, i=i))
-            # inp.font_size = inp.size[1]
+            inp.bind(text=partial(cpu_on_arrival, i=i))
             box.add_widget(inp)
             # burst times
             inp = TextInput(id='burst'+str(i))
-            inp.bind(text=partial(on_burst, i=i))
-            # inp.font_size = inp.size[1]
+            inp.bind(text=partial(cpu_on_burst, i=i))
             box.add_widget(inp)
 
             # If Priority scheduling selected
             if self.cpu_type == 4 or self.cpu_type == 5:
                 inp = TextInput(id='priority'+str(i))
-                inp.bind(text=partial(on_priority, i=i))
+                inp.bind(text=partial(cpu_on_priority, i=i))
                 box.add_widget(inp)
 
             layout.add_widget(box)
@@ -224,7 +196,7 @@ class CPUInputScreen(Screen):
         if self.cpu_type == 1:
             box = BoxLayout(orientation='horizontal', padding=(50,0))
             inp = TextInput(id='quantum')
-            inp.bind(text=on_quantum)
+            inp.bind(text=cpu_on_quantum)
             # inp.font_size = inp.size[1]
             label = Label(text='Time quantum')
             box.add_widget(label)
@@ -234,7 +206,7 @@ class CPUInputScreen(Screen):
         elif self.cpu_type == 4 or self.cpu_type == 5:
             box = BoxLayout(orientation='horizontal', padding=(50,0))
             inp = TextInput(id='aging')
-            inp.bind(text=on_aging)
+            inp.bind(text=cpu_on_aging)
             # inp.font_size = inp.size[1]
             label = Label(text='Aging - Promote priority by 1 unit after time: ')
             # label.text_size = label.size
@@ -242,6 +214,7 @@ class CPUInputScreen(Screen):
             box.add_widget(inp)
             layout.add_widget(box)
 
+# Output Screen for CPU Scheduling algorithms
 class CPUOutputScreen(Screen):
     layout = ObjectProperty(None)
     gantt = ObjectProperty(None)
@@ -365,13 +338,103 @@ class CPUOutputScreen(Screen):
         # Add the widget used to draw the gantt chart on the screen
         gantt.add_widget(chart_wid)
 
-# Create the screen manager
+# Input Screen for Deadlock Avoidance algorithm
+class DeadlockAvoidanceInputScreen(Screen):
+    form = ObjectProperty(None)
+    request_form = ObjectProperty(None)
+    def load_form(self, *args):
+        form = self.manager.get_screen('da_form').form
+        form.clear_widgets()
+        if (self.num_processes.text == "" or int(self.num_processes.text) < 1):
+            self.num_processes.text = "4"
+        if (self.num_resource_types.text == "" or int(self.num_resource_types) < 1):
+            self.num_resource_types.text = "4"
+
+        # Number of processes
+        n = int(self.num_processes.text)
+        # Number of resource types
+        m = int(self.num_resource_types.text)
+
+        # Add form labels for Available array (n)
+        box = BoxLayout(orientation='horizontal', size_hint_y=0.07)
+        box.add_widget(Label(text='Available:'))
+        for i in range(m):
+            box.add_widget(Label(text=chr(ord('A')+i)))
+        form.add_widget(box)
+
+        # Add input fields for Available array (n)
+        box = BoxLayout(orientation='horizontal', size_hint_y=0.07)
+        box.add_widget(Label(text=''))
+        for i in range(m):
+            inp = TextInput(id='available'+str(i))
+            inp.bind(text=partial(da_on_available, i=i))
+            box.add_widget(inp)
+        form.add_widget(box)
+
+        # Max Matrix (n x m)
+        # Add form labels for resource types
+        box = BoxLayout(orientation='horizontal', size_hint_y=0.07)
+        box.add_widget(Label(text='Max:'))
+        for i in range(m):
+            box.add_widget(Label(text=chr(ord('A')+i)))
+        form.add_widget(box)
+
+        # Add input fields for Max matrix (n x m)
+        vert_box = BoxLayout(orientation='vertical', size_hint_y=0.4)
+        for i in range(n):
+            box = BoxLayout(orientation='horizontal', size_hint_y=0.07)
+            box.add_widget(Label(text='P'+str(i+1)))
+            for j in range(m):
+                inp = TextInput(id='max'+str(i)+':'+str(j))
+                inp.bind(text=partial(da_on_max, i=i, j=j))
+                box.add_widget(inp)
+            vert_box.add_widget(box)
+        form.add_widget(vert_box)
+
+        # Allocation Matrix (n x m)
+        # Add form labels for resource types
+        box = BoxLayout(orientation='horizontal', size_hint_y=0.07)
+        box.add_widget(Label(text='Allocation:'))
+        for i in range(m):
+            box.add_widget(Label(text=chr(ord('A')+i)))
+        form.add_widget(box)
+
+        # Add input fields for Allocation matrix (n x m)
+        vert_box = BoxLayout(orientation='vertical', size_hint_y=0.4)
+        for i in range(n):
+            box = BoxLayout(orientation='horizontal', size_hint_y=0.07)
+            box.add_widget(Label(text='P'+str(i+1)))
+            for j in range(m):
+                inp = TextInput(id='allocation'+str(i)+':'+str(j))
+                inp.bind(text=partial(da_on_allocation, i=i, j=j))
+                box.add_widget(inp)
+            vert_box.add_widget(box)
+        form.add_widget(vert_box)
+
+        # Add labels for resource types in request form:
+        request_form = self.manager.get_screen('da_form').request_form
+        request_form.clear_widgets()
+        box = BoxLayout(orientation='horizontal')
+        box.add_widget(Label(text='Request:'))
+        for i in range(m):
+            box.add_widget(Label(text=chr(ord('A')+i)))
+        request_form.add_widget(box)
+
+        # Add input fields for resource form
+        box = BoxLayout(orientation='horizontal')
+        box.add_widget(Label(text=''))
+        for i in range(m):
+            inp = TextInput(id='request'+str(i))
+            inp.bind(text=partial(da_on_request, i=i))
+            box.add_widget(inp)
+        request_form.add_widget(box)
+
+# Create the screen manager and add all screens to it
 sm = ScreenManager()
 sm.add_widget(MainMenuScreen(name='menu'))
 sm.add_widget(CPUInputScreen(name='cpu_form'))
-#sm.add_widget(CPUSchedulingScreen(name='cpu1'))
-#sm.add_widget(CPUInputScreen_old(name='cpu2'))
 sm.add_widget(CPUOutputScreen(name='cpu_output'))
+sm.add_widget(DeadlockAvoidanceInputScreen(name='da_form'))
 
 class OSASK(App):
     def build(self):
