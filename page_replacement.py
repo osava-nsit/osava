e9f6edab69d8a4ca34384d85f3b4a0b393da04e9
Priyanka Singla <priyankasingla200@gmail.com>
2016-10-18 22:38:49 +0530

    Added least recenty used, least frequently used and most frequently used page replacement algorithms in page_replacement.py

--- page_replacement.py
+++ page_replacement.py
@@ -61,6 +61,121 @@ def find_and_replace_page_optimal(page_number, memory_frames, reference_string, 
     status = (frame_to_be_replaced + 1 , deepcopy(memory_frames));
     return status
 
+# Funtion to find and replace a page using lru algorithm 
+def find_and_replace_page_lru(page_number, memory_frames, reference_string, pos_ref_str):
+    # To keep track of page number with nearest past reference
+    frame_to_be_replaced = -1
+    flag = 0 # To check if an empty frame is available
+    
+    past_reference_pos = [] # To keep track of each page's nearest past reference
+    for frame_number, page in enumerate(memory_frames):
+        last_reference_pos = -1
+        for idx, page_n in enumerate(reference_string[0:pos_ref_str]):
+             # Empty frame is available
+            if page == -1:
+                frame_to_be_replaced = frame_number
+                flag = 1
+                break
+            elif page == page_n:
+                last_reference_pos = idx
+        if flag == 1:
+            break
+        pair =(frame_number, page, last_reference_pos);
+        past_reference_pos.append(pair)
+
+     # To find page with farthest reference in past
+    min_pos = pos_ref_str
+    if flag == 0:
+        for pair in past_reference_pos:
+            frame_number, page, last_reference_pos = pair
+            if min_pos > last_reference_pos:
+                min_pos = last_reference_pos
+                frame_to_be_replaced = frame_number
+            elif min_pos == last_reference_pos:
+                min_pos = last_reference_pos
+                frame_to_be_replaced =randint(0,len(memory_frames)-1)
+
+    memory_frames[frame_to_be_replaced] = page_number
+    status = (frame_to_be_replaced + 1 , deepcopy(memory_frames));
+    return status
+
+# Funtion to find and replace a page using lfu algorithm 
+def find_and_replace_page_least_frequently_used(page_number, memory_frames, reference_string, pos_ref_str):
+    # To keep track of page number with minimum reference in past
+    frame_to_be_replaced = -1
+    flag = 0 # To check if an empty frame is available
+    
+    freq_reference_pos = [] # To keep track of each page's number of references
+    for frame_number, page in enumerate(memory_frames):
+        num_reference_pos = 0
+        for idx, page_n in enumerate(reference_string[0:pos_ref_str]):
+             # Empty frame is available
+            if page == -1:
+                frame_to_be_replaced = frame_number
+                flag = 1
+                break
+            elif page == page_n:
+                num_reference_pos += 1
+        if flag == 1:
+            break
+        pair =(frame_number, page, num_reference_pos);
+        freq_reference_pos.append(pair)
+
+     # To find page with minimum reference in past
+    min_pos = pos_ref_str
+    if flag == 0:
+        for pair in freq_reference_pos:
+            frame_number, page, num_reference_pos = pair
+            if min_pos > num_reference_pos:
+                min_pos = num_reference_pos
+                frame_to_be_replaced = frame_number
+            elif min_pos == num_reference_pos:
+                min_pos = num_reference_pos
+                frame_to_be_replaced =randint(0,len(memory_frames)-1)
+
+    memory_frames[frame_to_be_replaced] = page_number
+    status = (frame_to_be_replaced + 1 , deepcopy(memory_frames));
+    return status
+
+# Funtion to find and replace a page using mfu algorithm 
+def find_and_replace_page_most_frequently_used(page_number, memory_frames, reference_string, pos_ref_str):
+    # To keep track of page number with maximum reference in past
+    frame_to_be_replaced = -1
+    flag = 0 # To check if an empty frame is available
+    
+    freq_reference_pos = [] # To keep track of each page's number of references
+    for frame_number, page in enumerate(memory_frames):
+        num_reference_pos = 0
+        for idx, page_n in enumerate(reference_string[0:pos_ref_str]):
+             # Empty frame is available
+            if page == -1:
+                frame_to_be_replaced = frame_number
+                flag = 1
+                break
+            elif page == page_n:
+                num_reference_pos += 1
+        if flag == 1:
+            break
+        pair =(frame_number, page, num_reference_pos);
+        freq_reference_pos.append(pair)
+
+     # To find page with maximum reference in past
+    max_pos = -1
+    if flag == 0:
+        for pair in freq_reference_pos:
+            frame_number, page, num_reference_pos = pair
+            if max_pos < num_reference_pos:
+                max_pos = num_reference_pos
+                frame_to_be_replaced = frame_number
+            elif max_pos == num_reference_pos:
+                max_pos = num_reference_pos
+                frame_to_be_replaced =randint(0,len(memory_frames)-1)
+
+    memory_frames[frame_to_be_replaced] = page_number
+    status = (frame_to_be_replaced + 1 , deepcopy(memory_frames));
+    return status
+
+
 # def fifo(page_number, memory_frames):
 #     allocated_frame, new_memory_frames = find_and_replace_page_fifo(page_number, memory_frames, last_replaced_frame)
 #     status = (allocated_frame, new_memory_frames);
@@ -100,15 +215,15 @@ def page_replacement(data):
             elif data['algo'] == 1:
                 allocated_frame, new_memory_frames = find_and_replace_page_optimal(page_number, memory_frames, page_numbers, pos)
             elif data['algo'] == 2:
-                allocated_frame, new_memory_frames = find_and_replace_page_lru(page_number, memory_frames)
+                allocated_frame, new_memory_frames = find_and_replace_page_lru(page_number, memory_frames, page_numbers, pos)
             elif data['algo'] == 3:
                 allocated_frame, new_memory_frames = find_and_replace_page_second_chance(page_number, memory_frames)
             elif data['algo'] == 4:
                 allocated_frame, new_memory_frames = find_and_replace_page_enhanced_second_chance(page_number, memory_frames)
             elif data['algo'] == 5:
-                allocated_frame, new_memory_frames = find_and_replace_page_least_recently_used(page_number, memory_frames)
+                allocated_frame, new_memory_frames = find_and_replace_page_least_frequently_used(page_number, memory_frames, page_numbers, pos)
             elif data['algo'] == 6:
-                allocated_frame, new_memory_frames = find_And_replace_page_most_recently_used(page_number, memory_frames)
+                allocated_frame, new_memory_frames = find_and_replace_page_most_frequently_used(page_number, memory_frames, page_numbers, pos)
             temp_memory['frame_number'] = allocated_frame
             temp_memory['memory_frames'] = new_memory_frames
             temp_memory['page_fault'] = 1
